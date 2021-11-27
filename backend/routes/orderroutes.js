87e4451b48fcb11553e010074d66a5878396bb36
manish <Manishbisht9711@gmail.com>
2021-11-27 14:15:57 +0530

    backend complete
    todo - cloudinary and payment methods

--- backend/routes/orderroutes.js
+++ backend/routes/orderroutes.js
@@ -4,20 +4,25 @@ const {
   getsingleorder,
   myorders,
   getallorders,
+  updateorder,
+  deleteorder,
 } = require("../controllers/order");
 const { isauthenticated, authorizedrole } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/order/new").put(isauthenticated, neworder);
 
-router
-  .route("/order/:id")
-  .get(isauthenticated, authorizedrole("admin"), getsingleorder);
+router.route("/order/:id").get(isauthenticated, getsingleorder);
 
 router.route("/myorder").get(isauthenticated, myorders);
 
 router
   .route("/allorders")
   .get(isauthenticated, authorizedrole("admin"), getallorders);
+
+router
+  .route("/order/:id")
+  .put(isauthenticated, authorizedrole("admin"), updateorder)
+  .delete(isauthenticated, authorizedrole("admin"), deleteorder);
 
 module.exports = router;
