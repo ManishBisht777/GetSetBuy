43754fde5f76f2079c7cc50edab683eb94402acc
manish <Manishbisht9711@gmail.com>
2021-11-28 21:47:44 +0530

    Navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
