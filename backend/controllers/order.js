93840e41bbc181e16ba45b3590fcbc6d57c3086e
manish <Manishbisht9711@gmail.com>
2021-11-29 22:54:38 +0530

    Revert "backend complete"
    This reverts commit 87e4451b48fcb11553e010074d66a5878396bb36.

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -1,7 +1,6 @@
 const Order = require("../models/ordermodel");
 const Errorhandler = require("../utils/errorhandler");
 const catchasyncerror = require("../middleware/catchasyncerror");
-const Product = require("../models/productmodel");
 
 // Create new Order
 
@@ -67,72 +66,15 @@ exports.myorders = catchasyncerror(async (req, res, next) => {
 
 exports.getallorders = catchasyncerror(async (req, res, next) => {
   const totalorders = await Order.countDocuments();
-  const orders = await Order.find().populate("user", "name email");
+  const order = await Order.find().populate("user", "name email");
 
-  let totalAmount = 0;
-
-  orders.forEach((order) => {
-    totalAmount += order.totalPrice;
-  });
+  if (!order) {
+    return next(new Errorhandler("Order not found with this Id", 404));
+  }
 
   res.status(200).json({
     success: true,
-    totalAmount,
     totalorders,
-    orders,
-  });
-});
-
-// update order
-
-exports.updateorder = catchasyncerror(async (req, res, next) => {
-  const order = await Order.findById(req.params.id);
-
-  if (!order) {
-    return next(new Errorhandler("Order not found with this Id", 404));
-  }
-
-  if (order.orderStatus === "delivered") {
-    return next(new Errorhandler("You have already delivered this order", 400));
-  }
-
-  if (req.body.status === "shipped") {
-    order.orderItems.forEach(async (o) => {
-      await updateStock(o.product, o.quantity);
-    });
-  }
-  order.orderStatus = req.body.status;
-
-  if (req.body.status === "delivered") {
-    order.deliveredAt = Date.now();
-  }
-
-  await order.save({ validateBeforeSave: false });
-  res.status(200).json({
-    success: true,
-  });
-});
-
-async function updateStock(id, quantity) {
-  const product = await Product.findById(id);
-
-  product.stock -= quantity;
-
-  await product.save({ validateBeforeSave: false });
-}
-
-// delete Order -- Admin
-
-exports.deleteorder = catchasyncerror(async (req, res, next) => {
-  const order = await Order.findById(req.params.id);
-
-  if (!order) {
-    return next(new Errorhandler("Order not found with this Id", 404));
-  }
-
-  await order.remove();
-
-  res.status(200).json({
-    success: true,
+    order,
   });
 });
