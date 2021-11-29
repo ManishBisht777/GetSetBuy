a9a646cacfcef3c7432d95a73c6b7c2a216460e7
manish <Manishbisht9711@gmail.com>
2021-11-29 23:08:45 +0530

    what am i doing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,7 @@
 import "./App.css";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import Navbar2 from "./components/Navbar2";
-import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-      <Navbar2 />
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-      <Footer />
-    </div>
-  );
+  return <div>hello</div>;
 }
 
 export default App;
