99c555aa8362d72faac9485a4eab191c8c9df955
manish <Manishbisht9711@gmail.com>
2021-12-31 22:40:11 +0530

    order success styling

--- frontend/src/components/cart/Ordersuccess.js
+++ frontend/src/components/cart/Ordersuccess.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import "./ordersuccess.css";
 
 const Ordersuccess = () => {
   return (
-    <div>
+    <div className="orderSuccess">
+      <CheckCircleIcon />
       <h3>Order success</h3>
       <Link to="/myorders">view order</Link>
     </div>
