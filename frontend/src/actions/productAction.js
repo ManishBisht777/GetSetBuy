e7e60021b0420e0c5781c262eda35d4960efa3a8
manish <Manishbisht9711@gmail.com>
2022-01-03 19:59:17 +0530

    error solved

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -128,7 +128,7 @@ export const getproductdetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
-    const { data } = await axios.get(`/api/products/${id}`);
+    const { data } = await axios.get(`/api/product/${id}`);
 
     if (data) {
       dispatch({
