c37f72d4686b3b45d91b564ef88e55fbc0f3f58e
manish <Manishbisht9711@gmail.com>
2022-01-07 20:32:15 +0530

    finalizing #3

--- frontend/src/components/user/Updatepassword.js
+++ frontend/src/components/user/Updatepassword.js
@@ -1,16 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { clearerror, updatepassword } from "../../actions/userAction";
 import { useAlert } from "react-alert";
 import { UPDATE_PASSWORD_RESET } from "../../constants/UserConstant";
+import LockOpenIcon from "@material-ui/icons/LockOpen";
+import LockIcon from "@material-ui/icons/Lock";
+import VpnKeyIcon from "@material-ui/icons/VpnKey";
+import Loader from "../layout/Loader/Loader";
+import "./changepassword.css";
 
 const Updatepassword = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const alert = useAlert();
 
-  const { error, isupdated } = useSelector((state) => state.profile);
+  const { loading, error, isupdated } = useSelector((state) => state.profile);
 
   const [oldpassword, setoldpassword] = useState("");
   const [newpassword, setnewpassword] = useState("");
@@ -41,35 +46,58 @@ const Updatepassword = () => {
   }, [alert, error, dispatch, navigate, isupdated]);
 
   return (
-    <div>
-      <h3>update password</h3>
-      <div className="sameform signupform">
-        <form action="" onSubmit={change}>
-          <h3>Signup</h3>
-          <input
-            // todo replace placeholder={ value} to value={ value}
-            name="oldpassword"
-            type="text"
-            placeholder="oldpassword"
-            onChange={(e) => setoldpassword(e.target.value)}
-          />
-          <input
-            name="newpassword"
-            type="text"
-            placeholder="newpassword"
-            onChange={(e) => setnewpassword(e.target.value)}
-          />
-          <input
-            name="confirmpassword"
-            type="text"
-            placeholder="confirmpassword"
-            onChange={(e) => setconfirmpassword(e.target.value)}
-          />
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <div className="updatePasswordContainer">
+            <div className="updatePasswordBox">
+              <h2 className="updatePasswordHeading">Update Profile</h2>
 
-          <input type="submit" value="Update" />
-        </form>
-      </div>
-    </div>
+              <form className="updatePasswordForm" onSubmit={change}>
+                <div className="loginPassword">
+                  <VpnKeyIcon />
+                  <input
+                    type="password"
+                    placeholder="Old Password"
+                    required
+                    value={oldpassword}
+                    onChange={(e) => setoldpassword(e.target.value)}
+                  />
+                </div>
+
+                <div className="loginPassword">
+                  <LockOpenIcon />
+                  <input
+                    type="password"
+                    placeholder="New Password"
+                    required
+                    value={newpassword}
+                    onChange={(e) => setnewpassword(e.target.value)}
+                  />
+                </div>
+                <div className="loginPassword">
+                  <LockIcon />
+                  <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    required
+                    value={confirmpassword}
+                    onChange={(e) => setconfirmpassword(e.target.value)}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  value="Change"
+                  className="updatePasswordBtn"
+                />
+              </form>
+            </div>
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
   );
 };
 
