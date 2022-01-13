f4a6bda8a3c212e1c75d2375eae8d9f67b3ecec5
manish <Manishbisht9711@gmail.com>
2022-01-13 21:35:24 +0530

    category img change

--- frontend/src/components/layout/category/Category.js
+++ frontend/src/components/layout/category/Category.js
@@ -8,17 +8,17 @@ const Category = () => {
       <div className="categories">
         <div className="category-card">
           <div className="category-card-info">
-            <h2>North India</h2>
+            <h2>Watches</h2>
             <p>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam,
               omnis? Atque dignissimos.
             </p>
-            <button className="view-now">Visit Now</button>
+            <button className="view-now">Shop Now</button>
           </div>
         </div>
         <div className="category-card">
           <div className="category-card-info">
-            <h2>East India</h2>
+            <h2>Electronics</h2>
             <p>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam,
               omnis? Atque dignissimos.
